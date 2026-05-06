7bf5be972e41420afc49e7d0f03817b96d1dec16
Javantea <jvoss@altsci.com>
2026-05-06 10:41:41 -0700

    Fix incbin for latest version of dasm.

--- tetris.s
+++ tetris.s
@@ -4824,22 +4824,22 @@ copyright_screen_background
         include backgrounds/copyright_screen.s
 
 title_screen_background
-        incbin backgrounds/title_screen.bin
+        incbin "backgrounds/title_screen.bin"
 
 game_select_screen_background
-        incbin backgrounds/game_select_screen.bin
+        incbin "backgrounds/game_select_screen.bin"
 
 level_select_screen_background
-        incbin backgrounds/level_select_screen.bin
+        incbin "backgrounds/level_select_screen.bin"
 
 ingame_screen_background
         include backgrounds/ingame_screen.s
 
 highscore_screen_background
-        incbin backgrounds/highscore_screen.bin
+        incbin "backgrounds/highscore_screen.bin"
 
 highscore_table_background
-        incbin backgrounds/highscore_table.bin
+        incbin "backgrounds/highscore_table.bin"
 
 a_type_menu_color_palette_background
         dc.b $3f, $0a, $01, $16
@@ -4851,13 +4851,13 @@ a_type_menu_color_palette_background
         dc.b $ff
 
 b_type_ending_lvl9_screen_background
-        incbin backgrounds/b_type_ending_lvl9_screen.bin
+        incbin "backgrounds/b_type_ending_lvl9_screen.bin"
 
 b_type_ending_screen_background
-        incbin backgrounds/b_type_ending_screen.bin
+        incbin "backgrounds/b_type_ending_screen.bin"
 
 type_a_ending_screen_background
-        incbin backgrounds/a_type_ending_screen.bin
+        incbin "backgrounds/a_type_ending_screen.bin"
 
 ;--------------------
 startPatchedRoutines
@@ -7390,4 +7390,4 @@ lbl_ff0f
         dc.b <resetHandler, >resetHandler    ; Reset vector
         dc.b <irqHandler, >irqHandler        ; IRQ/BRK vector
 
-        incbin pattern_tables.bin
+        incbin "pattern_tables.bin"
